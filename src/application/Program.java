f3323291c3f46f032e51e7a9c9d4d1d7ffad3ae0
Heliton Querioz <heliton.queiroz10@gmail.com>
2026-05-14 16:10:12 -0300

    Update implementação

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,5 +33,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "gred@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserido! novo Id = " + newSeller.getId());
+
+        System.out.println("\n=== Teste 5: seller update === ");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completo!");
     }
 }
